66ff3cbc972bf13a6c7b67ae70672d88fd48acad
mickael59b <mboutte621@gmail.com>
2024-10-12 16:39:52 +0200

    Update index.js

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,88 +4,50 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); });
+const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
 
 const Form = ({ onSuccess, onError }) => {
-  const [formData, setFormData] = useState({
-    nom: '',
-    prenom: '',
-    type: '',
-    email: '',
-    message: ''
-  });
-
   const [sending, setSending] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
       setSending(true);
+      // We try to call mockContactApi
       try {
-        await mockContactApi(); // Appeler l'API fictive
+        await mockContactApi();
         setSending(false);
-        onSuccess(); // Appeler onSuccess si tout s'est bien passé
+        // Rajout du onSuccess
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
       }
     },
+    // Le onSucess n'étais pas utilisé
     [onSuccess, onError]
   );
-
   return (
     <form onSubmit={sendContact}>
       <div className="row">
         <div className="col">
-          <Field
-            placeholder="Nom"
-            label="Nom"
-            name="nom"
-            value={formData.nom}
-            onChange={handleChange}
-          />
-          <Field
-            placeholder="Prénom"
-            label="Prénom"
-            name="prenom"
-            value={formData.prenom}
-            onChange={handleChange}
-          />
+          <Field placeholder="" label="Nom" />
+          <Field placeholder="" label="Prénom" />
           <Select
             selection={["Personel", "Entreprise"]}
+            onChange={() => null}
             label="Personel / Entreprise"
-            name="type"
-            value={formData.type}
-            onChange={handleChange}
             type="large"
             titleEmpty
           />
-          <Field
-            placeholder="Email"
-            label="Email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
+          <Field placeholder="" label="Email" />
           <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
             {sending ? "En cours" : "Envoyer"}
           </Button>
         </div>
         <div className="col">
           <Field
-            placeholder="Message"
+            placeholder="message"
             label="Message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
             type={FIELD_TYPES.TEXTAREA}
           />
         </div>
@@ -97,11 +59,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-};
+}
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-};
+}
 
 export default Form;
